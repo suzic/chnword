72e1f6ec58b138184bc1b1e355f9e4eb189246e8
kaikaiwei <137067401@qq.com>
2015-08-02 18:57:50 +0800

    商店bug fixed

--- chnword/ShopViewController.m
+++ chnword/ShopViewController.m
@@ -23,9 +23,10 @@
     [super viewDidLoad];
     
     // 设置背景图片
-    CGRect frame = self.view.frame;
-    frame.origin.y -= 64;
+    CGRect frame = self.view.bounds;
+    NSLog(@"%@", NSStringFromCGRect(frame));
     frame.size.height += 64;
+    
     UIImageView *bacgroundImageView = [[UIImageView alloc] initWithFrame:frame];
     [bacgroundImageView setImage:[UIImage imageNamed:@"Background"]];
     [self.view insertSubview:bacgroundImageView atIndex:0];
@@ -33,20 +34,35 @@
     self.shopSuit.layer.cornerRadius = 8.0f;
     self.shopAnime.layer.cornerRadius = 8.0f;
     self.shopCard.layer.cornerRadius = 8.0f;
+    
+//    self.tableView.frame=CGRectMake (0, 0, self.view.frame.size.width, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height);
+    
+    
+    
+    
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"BrandTitle"] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    [self.navigationItem setTitle:@"产品商店"];
+    
+    
+
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
     
-    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"BrandTitle"] forBarMetrics:UIBarMetricsDefault];
-    [self.navigationController setNavigationBarHidden:NO animated:YES];
-    [self.navigationItem setTitle:@"产品商店"];
+    CGRect rect = self.tableView.frame;
+    rect.size.height -= 49;
+    self.tableView.frame = rect;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -72,48 +88,5 @@
     return 0.01f;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
